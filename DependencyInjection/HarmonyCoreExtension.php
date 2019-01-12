b3ac493010fb14ead1cf5bc5eb37bc2125ea4b70
David Sanchez <david38sanchez@gmail.com>
2019-01-11 23:25:04 -0500

    Update code, removing useless

--- DependencyInjection/HarmonyCoreExtension.php
+++ DependencyInjection/HarmonyCoreExtension.php
@@ -5,6 +5,7 @@ namespace Harmony\Bundle\CoreBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
  *
  * @package Harmony\Bundle\CoreBundle\DependencyInjection
  */
-class HarmonyCoreExtension extends Extension
+class HarmonyCoreExtension extends Extension implements PrependExtensionInterface
 {
 
     /** HarmonyCMS alias name */
@@ -29,9 +30,6 @@ class HarmonyCoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $config = $this->processConfiguration(new Configuration(), $configs);
-        $container->setParameter(HarmonyCoreExtension::ALIAS . '.settings', $config['settings']);
-
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
         $loader->load('services.yaml');
     }
@@ -44,5 +42,29 @@ class HarmonyCoreExtension extends Extension
     public function getAlias(): string
     {
         return self::ALIAS;
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        // get all bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        // Yaml loader
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+
+        if (isset($bundles['DoctrineBundle'])) {
+            $loader->load('doctrine.yaml');
+        }
+
+        if (isset($bundles['HelisSettingsManagerBundle'])) {
+            $loader->load('settings_manager.yaml');
+        }
     }
 }
