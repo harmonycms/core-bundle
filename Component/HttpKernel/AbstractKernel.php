45eeffda8c9c8926b3ca1f9251e2d33386c4daac
David Sanchez <david38sanchez@gmail.com>
2019-02-10 21:00:25 -0500

    Add code initializing themes discovery in kernel boot

--- Component/HttpKernel/AbstractKernel.php
+++ Component/HttpKernel/AbstractKernel.php
@@ -2,6 +2,8 @@
 
 namespace Harmony\Bundle\CoreBundle\Component\HttpKernel;
 
+use Harmony\Sdk\Theme\Theme;
+use Harmony\Sdk\Theme\ThemeInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 /**
@@ -22,6 +24,20 @@ abstract class AbstractKernel extends BaseKernel
 
     /** @var string */
     protected $appVersion = self::APP_VERSION;
+
+    /** @var ThemeInterface[] $themes */
+    protected $themes = [];
+
+    /**
+     * Boots the current kernel.
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // init themes
+        $this->initializeThemes();
+    }
 
     /**
      * Gets the application name of the kernel.
@@ -54,16 +70,48 @@ abstract class AbstractKernel extends BaseKernel
     }
 
     /**
+     * Returns an array of themes to register.
+     *
+     * @return iterable|ThemeInterface[] An iterable of theme instances
+     */
+    abstract public function registerThemes(): iterable;
+
+    /**
+     * Initializes themes.
+     *
+     * @throws \LogicException if two themes share a common name
+     */
+    protected function initializeThemes(): void
+    {
+        // init themes
+        $this->themes = [];
+        /** @var Theme $theme */
+        foreach ($this->registerThemes() as $theme) {
+            $name = $theme->getIdentifier();
+            if (isset($this->themes[$name])) {
+                throw new \LogicException(sprintf('Trying to register two themes with the same name "%s"', $name));
+            }
+            $this->themes[$name] = $theme;
+        }
+    }
+
+    /**
      * Returns the kernel parameters.
      *
      * @return array An array of kernel parameters
      */
     protected function getKernelParameters(): array
     {
+        $themes = [];
+        foreach ($this->themes as $name => $theme) {
+            $themes[$name] = \get_class($theme);
+        }
+
         return array_merge(parent::getKernelParameters(), [
             'kernel.app_name'    => $this->appName,
             'kernel.app_version' => $this->appVersion,
-            'kernel.theme_dir'   => $this->getThemeDir()
+            'kernel.theme_dir'   => $this->getThemeDir(),
+            'kernel.themes'      => $themes
         ]);
     }
 }
