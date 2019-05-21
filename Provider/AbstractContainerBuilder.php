673a7793f3de6f185471d7e74a0501d17aeeba19
David Sanchez <david.sanchez@nventive.com>
2019-05-21 14:48:31 -0400

    Add abstract methods

--- Provider/AbstractContainerBuilder.php
+++ Provider/AbstractContainerBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Harmony\Bundle\CoreBundle\Provider;
 
+use PDOException;
 use ProxyManager\Configuration;
 use Symfony\Bridge\ProxyManager\LazyProxy\Instantiator\RuntimeInstantiator;
 use Symfony\Component\DependencyInjection\ContainerBuilder as BaseContainerBuilder;
@@ -29,4 +30,64 @@ abstract class AbstractContainerBuilder extends BaseContainerBuilder
             $this->setProxyInstantiator(new RuntimeInstantiator());
         }
     }
+
+    /**
+     * Compiles the container.
+     * This method passes the container to compiler
+     * passes whose job is to manipulate and optimize
+     * the container.
+     * The main compiler passes roughly do four things:
+     *  * The extension configurations are merged;
+     *  * Parameter values are resolved;
+     *  * The parameter bag is frozen;
+     *  * Extension loading is disabled.
+     *
+     * @param bool $resolveEnvPlaceholders Whether %env()% parameters should be resolved using the current
+     *                                     env vars or be replaced by uniquely identifiable placeholders.
+     *                                     Set to "true" when you want to use the current ContainerBuilder
+     *                                     directly, keep to "false" when the container is dumped instead.
+     */
+    public function compile(bool $resolveEnvPlaceholders = false)
+    {
+        try {
+            $this->initConnection();
+            $this->addDbParameters();
+            $this->addDbConfig();
+            $this->closeConnection();
+        }
+        catch (PDOException $e) {
+            parent::compile($resolveEnvPlaceholders);
+
+            return;
+        }
+        parent::compile($resolveEnvPlaceholders);
+    }
+
+    /**
+     * Initializes the database connection
+     *
+     * @return void
+     */
+    abstract protected function initConnection(): void;
+
+    /**
+     * Adds the parameters from the database to the container's parameterBag
+     *
+     * @return void
+     */
+    abstract protected function addDbParameters(): void;
+
+    /**
+     * Adds configs from the database to the current configs
+     *
+     * @return void
+     */
+    abstract protected function addDbConfig(): void;
+
+    /**
+     * Closes the database connection
+     *
+     * @return void
+     */
+    abstract protected function closeConnection(): void;
 }
--- Provider/ContainerBuilderOrm.php
+++ Provider/ContainerBuilderOrm.php
@@ -10,4 +10,43 @@ namespace Harmony\Bundle\CoreBundle\Provider;
 class ContainerBuilderOrm extends AbstractContainerBuilder
 {
 
+    /**
+     * Initializes the database connection
+     *
+     * @return void
+     */
+    protected function initConnection(): void
+    {
+        // TODO: Implement initConnection() method.
+    }
+
+    /**
+     * Adds the parameters from the database to the container's parameterBag
+     *
+     * @return void
+     */
+    protected function addDbParameters(): void
+    {
+        // TODO: Implement addDbParameters() method.
+    }
+
+    /**
+     * Adds configs from the database to the current configs
+     *
+     * @return void
+     */
+    protected function addDbConfig(): void
+    {
+        // TODO: Implement addDbConfig() method.
+    }
+
+    /**
+     * Closes the database connection
+     *
+     * @return void
+     */
+    protected function closeConnection(): void
+    {
+        // TODO: Implement closeConnection() method.
+    }
 }
