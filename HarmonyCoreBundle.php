571c78ff593d23c9b299260b5eed8e69f8e2d12d
David Sanchez <david38sanchez@gmail.com>
2019-05-21 22:07:56 -0400

    Add and loading doctrine mapping XML configuration

--- HarmonyCoreBundle.php
+++ HarmonyCoreBundle.php
@@ -2,9 +2,15 @@
 
 namespace Harmony\Bundle\CoreBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
 use Harmony\Bundle\CoreBundle\DependencyInjection\HarmonyCoreExtension;
+use LogicException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use function class_exists;
+use function realpath;
 
 /**
  * Class HarmonyCoreBundle
@@ -18,11 +24,24 @@ class HarmonyCoreBundle extends Bundle
     const VERSION = '0.1';
     const NAME    = 'HarmonyCMS';
 
+    public function build(ContainerBuilder $container)
+    {
+        // get all bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        $mappings = [realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'Harmony\Bundle\CoreBundle\Model'];
+        if (class_exists(DoctrineMongoDBMappingsPass::class) && isset($bundles['DoctrineMongoDBBundle'])) {
+            $container->addCompilerPass(DoctrineMongoDBMappingsPass::createXmlMappingDriver($mappings, []));
+        } elseif (class_exists(DoctrineOrmMappingsPass::class) && isset($bundles['DoctrineBundle'])) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
+    }
+
     /**
      * Returns the bundle's container extension.
      *
      * @return ExtensionInterface|null The container extension
-     * @throws \LogicException
+     * @throws LogicException
      */
     public function getContainerExtension(): ExtensionInterface
     {
